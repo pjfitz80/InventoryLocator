de4f91bc281d9a9e6f2a3f93041d22614f60f565
Patrick Fitzgerald <pjfitz80@gmail.com>
2018-03-16 22:57:49 -0700

    Add files via upload
    Removed more code for dialogs no longer needed.

--- main/java/com/example/patrick/myinventorylocator/utility/MyDialogs.java
+++ main/java/com/example/patrick/myinventorylocator/utility/MyDialogs.java
@@ -31,7 +31,6 @@ public class MyDialogs {
         alertDialogBuilder = new AlertDialog.Builder(mContext);
     }
 
-
     /** A dialog to handle the event of the user entering a stock number
      * that is not yet part of the inventory.
      */
@@ -50,27 +49,6 @@ public class MyDialogs {
         alertDialog.show();
     }
 
-    /**
-     * A dialog for controlling the flow during vehicle location scanning.
-     */
-    public void scanVehicleLocationDialog() {
-        alertDialogBuilder.setTitle(R.string.vehicle_location_scan_dialog_title);
-        alertDialogBuilder.setMessage(R.string.vehicle_location_scan_dialog_message)
-                .setCancelable(false)
-                .setPositiveButton(R.string.vehicle_location_scan_dialog_positive,new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        dialog.cancel();
-                    }
-                })
-                .setNegativeButton(R.string.vehicle_location_scan_dialog_negative,new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        returnToMapActivity();
-                    }
-                });
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }
-
     /** A dialog for editing a Vehicles attributes. WIll make use of
      * the Vehicle object's getters/setters to fill in previously null fields.
      */
@@ -84,13 +62,5 @@ public class MyDialogs {
     private void dialogSound() {
         mp = MediaPlayer.create(mContext, R.raw.mario_negative);
         mp.start();
-    }
-
-    /**
-     *  A method that start the MapsActivity.
-     */
-    private void returnToMapActivity() {
-        Intent mIntent = new Intent(mContext, MapsActivity.class);
-        mContext.startActivity(mIntent);
     }
 }
